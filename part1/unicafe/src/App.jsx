b9c1f70bea2049b10e47cb3b4e2e58e9dcd5a595
hunterb2121 <hunterb01@outlook.com>
2024-05-25 12:06:27 -0700

    FullStackOpen 1.11: unicafe step 6

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -4,27 +4,35 @@ const Button = (props) => <button onClick={props.handleClick}>{props.text}</butt
 
 const Display = (props) => <span>{props.value}</span>;
 
-const StatisticLine = (props) => <p>{props.text}: {props.value}</p>
+const StatisticLine = (props) => {
+  return (
+    <tr>
+      <td>{props.text}</td>
+      <td>{props.value}</td>
+    </tr>
+  );
+};
 
 const Statistics = (props) => {
   const { g, n, b } = props;
 
   const total = [g, n, b].reduce((acc, currValue) => acc + currValue, 0);
 
-  const averageFeedback = () => total !== 0 ? ((g * 1) + (n * 0) + (b * -1)) / total : 0;
-  const positiveFeedback = () => total !== 0 ? (g / total * 100) : 0;
+  const averageFeedback = () => total !== 0 ? (((g * 1) + (n * 0) + (b * -1)) / total).toFixed(2) : 0;
+  const positiveFeedback = () => total !== 0 ? (g / total * 100).toFixed(2) : 0;
 
   if (total !== 0) {
     return (
-      <div>
-        <h2>Statistics</h2>
-        <StatisticLine text="Good" value={g} />
-        <StatisticLine text="Neutral" value={n} />
-        <StatisticLine text="Bad" value={b} />
-        <StatisticLine text="Total" value={total} />
-        <StatisticLine text="Average" value={averageFeedback()} />
-        <StatisticLine text="Positive" value={`${positiveFeedback()}%`} />
-      </div>
+      <table>
+        <tbody>
+          <StatisticLine text="Good" value={g} />
+          <StatisticLine text="Neutral" value={n} />
+          <StatisticLine text="Bad" value={b} />
+          <StatisticLine text="Total" value={total} />
+          <StatisticLine text="Average" value={averageFeedback()} />
+          <StatisticLine text="Positive" value={`${positiveFeedback()}%`} />
+        </tbody>
+      </table>
     );
   } else {
     return (
@@ -59,6 +67,8 @@ const App = () => {
       <Button handleClick={() => {increase("g")}} text="Good" />
       <Button handleClick={() => {increase("n")}} text="Neutral" />
       <Button handleClick={() => {increase("b")}} text="Bad" />
+
+      <h2>Statistics</h2>
 
       <Statistics g={good} b={bad} n={neutral} />
     </div>
