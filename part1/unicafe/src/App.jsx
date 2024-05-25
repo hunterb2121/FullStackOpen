878985cd355fa8dbf11cd5306ee0db68d36d83f1
hunterb2121 <hunterb01@outlook.com>
2024-05-25 10:01:45 -0700

    FullStackOpen 1.9: unicafe step 4

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -12,17 +12,26 @@ const Statistics = (props) => {
   const averageFeedback = () => total !== 0 ? ((g * 1) + (n * 0) + (b * -1)) / total : 0;
   const positiveFeedback = () => total !== 0 ? (g / total * 100) : 0;
 
-  return (
-    <div>
-      <h2>Statistics</h2>
-      <p>Good: <Display value={g} /></p>
-      <p>Neutral: <Display value={n} /></p>
-      <p>Bad: <Display value={b} /></p>
-      <p>Total: <Display value={total} /></p>
-      <p>Average: <Display value={averageFeedback()} /></p>
-      <p>Positive: <Display value={positiveFeedback()} />%</p>
-    </div>
-  );
+  if (total !== 0) {
+    return (
+      <div>
+        <h2>Statistics</h2>
+        <p>Good: <Display value={g} /></p>
+        <p>Neutral: <Display value={n} /></p>
+        <p>Bad: <Display value={b} /></p>
+        <p>Total: <Display value={total} /></p>
+        <p>Average: <Display value={averageFeedback()} /></p>
+        <p>Positive: <Display value={positiveFeedback()} />%</p>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <h2>Statistics</h2>
+        <p>No Feedback Given</p>
+      </div>
+    );
+  }
 };
 
 const App = () => {
@@ -40,8 +49,6 @@ const App = () => {
       setBad(bad + 1);
     }
   };
-
-  
 
   return (
     <div>
