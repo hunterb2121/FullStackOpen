c81904f92a3ebcedc453d374b11427ef4367cc89
hunterb2121 <hunterb01@outlook.com>
2024-05-28 11:52:43 -0700

    FullStackOpen 2.1: Course Information step6

---
+++ part2/courseinformation/src/App.jsx
@@ -0,0 +1,49 @@
+const Header = ({ course }) => <h1>{course}</h1>;
+
+const Total = ({ sum }) => <p>Number of exercises {sum}</p>
+
+const Part = ({ part }) => <p>{part.name} {part.exercises}</p>;
+
+const Content = ({ parts }) => {
+  return (
+    <>
+      {parts.map(part => <Part part={part} />)}
+    </>
+  );
+};
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+    </div>
+  );
+};
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: "Half Stack Application Development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7
+      },
+      {
+        name: "State of a component",
+        exercises: 14
+      },
+    ]
+  };
+
+  return (
+    <Course course={course} />
+  );
+};
+
+export default App;
